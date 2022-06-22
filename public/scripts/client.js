076d2ef94d1e9395158b396ace345ea2788feea3
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-22 23:49:38 +0000

    Add load tweet and time-ago function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,44 +1,15 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
-const renderTweets = function(data) {
-  data.forEach(element => {
-    const tweet = createTweetElement(element);
-    $("#tweet-holder").append(tweet);
-  });
-};
+$(document).ready(function() {
+  const renderTweets = function(data) {
+    data.forEach(element => {
+      const tweet = createTweetElement(element);
+      $("#tweet-holder").append(tweet);
+    });
+  };
 
-const createTweetElement = function(data) {
-  const tweet =
-    `<article class="tweet-display">
+  const createTweetElement = function(data) {
+    const tweet =
+      `<article class="tweet-display">
     <header class="head">
       <div class="avatar-name">
         <div class="avatar-pic">
@@ -50,7 +21,7 @@ const createTweetElement = function(data) {
     </header>
     <p class="summary">${data.content.text}</p>
     <footer class="foot">
-      <p>${data.created_at}</p>
+      <p>${timeago.format(data.created_at)}</p>
       <div class="key">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -58,14 +29,19 @@ const createTweetElement = function(data) {
       </div>
     </footer>
   </article>`;
-  return tweet;
-};
+    return tweet;
+  };
 
-$(document).ready(function() {
-  renderTweets(tweetData);
+
   $('form').submit((event) => {
     event.preventDefault();
     const data = $("textarea").serialize();
     $.post("/tweets", data);
   });
+  const loadTweets = function() {
+    $.get("/tweets", (data) => {
+      renderTweets(data);
+    });
+  };
+  loadTweets();
 });
